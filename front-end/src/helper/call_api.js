da4231a3f3b5d807f13a718cc2088ed2be3f06f3
HaThiMyChi <mychikhtn@gmail.com>
2020-04-18 18:25:19 +0700

    Update call_api.js
    create page admin

--- front-end/src/helper/call_api.js
+++ front-end/src/helper/call_api.js
@@ -4,7 +4,7 @@ var md5 = require('md5');
 module.exports = {
     // Thời gian sử dụng token lần gần nhất
     last_usded: 0,
-    base_url: 'https://localhost:44396/api/',
+    base_url: 'https://localhost:5001/api/',
     secret_key: '75836f6ded2047c4b1f5770c3229fc02',
     token_time: 100,
 
@@ -15,8 +15,7 @@ module.exports = {
             // lấy access_token mới và lưu lại
             // TODO
             access_token_new = localStorage.getItem('access_token')
-        }
-        else {
+        } else {
             // dùng access_token hiện tại
             access_token_new = localStorage.getItem('access_token')
         }
@@ -28,8 +27,7 @@ module.exports = {
         var hash = "";
         if (method === "get") {
             hash = md5("" + this.secret_key + timestamp)
-        }
-        else {
+        } else {
             hash = md5(JSON.stringify(request) + this.secret_key + timestamp)
         }
 
@@ -51,8 +49,7 @@ module.exports = {
         } else if (method === "put") {
             // TODO
             console.log(this.last_usded)
-        }
-        else if (method === "post") {
+        } else if (method === "post") {
             this.last_usded = Math.round((new Date()).getTime() / 1000);
             return new Promise((resolve, reject) => {
                 instance.post(url, request)
