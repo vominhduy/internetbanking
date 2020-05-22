e563959039af53d1983c227881d6b466cdcaaecb
htdat0202 <htdat0202@gmail.com>
2020-05-22 20:18:59 +0700

    Update helper: show notification message

--- front-end/src/helper/helper.js
+++ front-end/src/helper/helper.js
@@ -8,4 +8,26 @@ module.exports = {
 
         return JSON.parse(jsonPayload);
     },
+    showSuccessfullMsg(scope, msg) {
+        scope.$bvModal.msgBoxOk(msg, {
+            title: 'Thông báo',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'success',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+        });
+    },
+    showErrorMsg(scope, msg) {
+        scope.$bvModal.msgBoxOk(msg, {
+            title: 'Thông báo',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'danger',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+        });
+    },
 }
