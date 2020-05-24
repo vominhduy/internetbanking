81b4815c21459a358f447d72944158cc2e0f31c0
htdat0202 <htdat0202@gmail.com>
2020-05-24 19:34:20 +0700

    no message

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -13,7 +13,7 @@ import EmployeeHome from '../views/Employee/Home.vue'
 import UserHome from '../views/User/Home.vue'
 import UserAccounts from '../views/User/UserAccounts.vue'
 import UserContacts from '../views/User/Contacts.vue'
-import UserHistories from '../views/User/History.vue'
+import UserHistories from '../views/User/UserHistory.vue'
 import UserDeptreminders from '../views/User/Deptreminders.vue'
 import UserListDeptreminders from '../views/User/ListDeptreminders.vue'
 
